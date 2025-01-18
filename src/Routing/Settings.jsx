3373b60be0f16d13347c9f86ee0192358996aac8
Anchie <wyhwtf@gmail.com>
2025-01-18 13:57:37 +0100

    Made functionality

--- src/Routing/Settings.jsx
+++ src/Routing/Settings.jsx
@@ -3,19 +3,59 @@ import { SessionContext } from "../Contexts/SessionContext"
 import PopUp from "../Components/PopUp/Popup"
 import { PopUpContext } from "../Contexts/PopUpContext"
 import { PopUpOptionContext } from "../Contexts/PopUpOptionContext"
-
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 function Settings()
 { 
 
-  const {userSession} = useContext(SessionContext)
+  const {userSession, setSession} = useContext(SessionContext)
   const {setPopUpStatus} = useContext(PopUpContext)
   const {setPopUpOption} = useContext(PopUpOptionContext)
+  const navigate = useNavigate()
 
   const showPopup = (PopUpLabelText, PopUphtmlFor, PopUpPlaceholder, PopUpId) => {
     setPopUpStatus("opacity-1 transition-all pointer-events-auto size-full mb-19 absolute bg-transparent-300")
     setPopUpOption({labelText: PopUpLabelText, htmlFor: PopUphtmlFor, placeholder: PopUpPlaceholder, id: PopUpId})
     document.body.classList.add("no-scroll")
+  }
+
+  const handleLogOut = (e) => {
+    e.preventDefault()
+
+    axios
+    .delete("http://localhost/shopiffy/server/endpoints/logout.php", {
+      withCredentials: true
+    })
+    .then(response => {
+      if(response.data.status == "success") {
+        setSession({username:null, email:null})
+        navigate("/")
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  const handleDeleteAccount = (e) => {
+    e.preventDefault()
+
+    axios
+    .delete("http://localhost/shopiffy/server/endpoints/deleteaccount.php", {
+      withCredentials: true
+    })
+    .then(response => {
+      console.log(response.data)
+      if(response.data.status == "success") {
+        setSession({username:null, email:null})
+        navigate("/")
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
   }
 
 
@@ -69,19 +109,19 @@ function Settings()
 
           <div className="flex flex-row justify-around mt-10">
             
-            <form>
+            <form onSubmit={handleLogOut} method="delete">
               <button type="submit" className="font-Manrope cursor-pointer text-lg bg-softBrown-200 border-2 border-softBrown-300 
                   py-2 px-8 rounded-md transition-opacity hover:opacity-70">
                     Log out
               </button>
             </form>
 
-            <form>
+            <form onSubmit={handleDeleteAccount} method="delete">
               <button type="submit" className="font-Manrope cursor-pointer text-lg bg-softBrown-200 border-2 border-softBrown-300 
                   py-2 px-8 rounded-md transition-opacity hover:opacity-70">
                     Delete account
               </button>
-              <input type="hidden" name="username" value={userSession.username}/>
+              <p className="text-xs mt-2">WARNING! this option is permament</p>
             </form>
           </div>
         </div>
